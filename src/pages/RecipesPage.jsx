3adb99bcc17ba4c1bbae284be6338400b92eab8c
bilodav <bilodavids@gmail.com>
2026-08-26 23:22:00 +0200

    Added some recipe images and videos

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,8 +1,30 @@
+import { recipesData } from "../data/recipesData";
+
 function RecipesPage() {
   return (
-    <>
+    <section
+      style={{ marginLeft: "50px", maxHeight: "100vh", overflow: "auto" }}
+    >
       <h1>recipes</h1>
-    </>
+      {recipesData.map((recipe) => (
+        <>
+          <p>{recipe.id}</p>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.cookTime}</p>
+          <img src={`../src/${recipe.image}`} alt="food" />
+          <ul style={{ listStyleType: "disc" }}>
+            {recipe.ingredients.map((ing) => (
+              <li>{ing}</li>
+            ))}
+          </ul>
+          <ol style={{ listStyleType: "number", marginBottom: "20px" }}>
+            {recipe.instructions.map((ing) => (
+              <li>{ing}</li>
+            ))}
+          </ol>
+        </>
+      ))}
+    </section>
   );
 }
 
